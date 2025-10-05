cac710c997e6244e6b9be5eb052d2d253fadcba0
Willian Amorim <willianamorim1@outlook.com.br>
2025-10-05 12:47:03 -0300

    adicionado alterações em spaces

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: process.env.NEXT_PUBLIC_API_URL || "https://coworking-back-production.up.railway.app/api",
-  baseURL: process.env.NEXT_PUBLIC_API_URL_PROD,
-  // baseURL: process.env.NEXT_PUBLIC_API_URL_LOCAL,
+  // baseURL: process.env.NEXT_PUBLIC_API_URL_PROD,
+  baseURL: process.env.NEXT_PUBLIC_API_URL_LOCAL,
    headers: {
     'Content-Type': 'application/json',
   },
