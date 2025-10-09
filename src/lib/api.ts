4c16587cbb9c12fc4ea4b676fb7d8ae132b4d1e8
Willian Amorim <willianamorim1@outlook.com.br>
2025-10-09 13:26:10 -0300

    alterado a rota do back

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -6,7 +6,7 @@ const baseURL =
     : process.env.NEXT_PUBLIC_API_URL_PROD;
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL_LOCAL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL_PROD,
   headers: {
     'Content-Type': 'application/json',
   },
