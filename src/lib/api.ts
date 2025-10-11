8d0815d513078a3d4bc20a69c4afaba60e06ca2a
Willian Amorim <willianamorim1@outlook.com.br>
2025-10-11 00:05:28 -0300

    adicionado alterações

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -6,8 +6,8 @@ import axios from "axios";
 //     : process.env.NEXT_PUBLIC_API_URL_PROD;
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL_LOCAL,
-  // baseURL: process.env.NEXT_PUBLIC_API_URL_PROD,
+  // baseURL: process.env.NEXT_PUBLIC_API_URL_LOCAL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL_PROD,
   headers: {
     'Content-Type': 'application/json',
   },
